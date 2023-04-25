b52a53ad5367a9467eef7374bcdf4f66386b052e
Bruna Cedro <bruna.cedro70@gmail.com>
2023-04-25 20:21:32 -0300

    nova pasta com projetod DIO

---
+++ Trilha-Java-Basico/TiposeVariaveis/src/main/java/exercicios/DeclaracaoVariaveis.java
@@ -0,0 +1,37 @@
+package exercicios;
+
+public class DeclaracaoVariaveis {
+
+	public static void main(String[] args) {
+	
+		int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+		int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+		double salarioMinimo = 2500; //tipo "double", nome "salarioMinimo", valor 2500.
+		
+		//Atenção: existe algumas peculiaridades a trabalhar com alguns tipos específicos. Observe no exemplo abaixo:
+		
+		byte anos = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F; // para evitar erro de compilacao sempre colocar um f no final 
+		double salario = 1275.33;
+		
+		/*
+		 * Observe que o tipo long precisa terminar com L, o tipo float precisa terminar
+		 * com F e alguns cenários do dia-a-dia podem estimular uma alteração de tipos
+		 * de dados convencional.
+		 */
+		
+		
+		// TiposEVariaveis.java
+
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+	}
+	
+	
+	
+
+}
